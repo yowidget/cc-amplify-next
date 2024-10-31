60853fe3fb681663b32d0d6f71c2876fb0ed1fa8
Arturo Vélez Badiola <widget@MacBook-Pro-de-Arturo-2.local>
2024-10-31 01:41:12 -0600

    con bedrock en /inference

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,22 +2,123 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
 import { Stack } from "aws-cdk-lib";
-import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
+import {
+  Policy,
+  Effect,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { StartingPosition, EventSourceMapping } from "aws-cdk-lib/aws-lambda";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import { aws_events } from "aws-cdk-lib";
 
 import { sayHello } from "./functions/say-hello/resource";
 import { myDynamoDBFunction } from "./functions/dynamoDB-function/resource";
 import { CustomNotifications } from "./custom/CustomNotifications/resource";
+import {  MODEL_ID, generateHaikuFunction } from "./data/resource";
 
 const backend = defineBackend({
   auth,
   data,
   sayHello,
   myDynamoDBFunction,
+  generateHaikuFunction
 });
- 
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
+// Create a new stack for the EventBridge data source
+const eventStack = backend.createStack("MyExternalDataSources");
+// Reference or create an EventBridge EventBus
+const eventBus = aws_events.EventBus.fromEventBusName(
+  eventStack,
+  "MyEventBus",
+  "default"
+);
+
+backend.data.addEventBridgeDataSource("MyEventBridgeDataSource", eventBus);
+
+// Create a policy statement to allow invoking the AppSync API's mutations
+const policyStatement = new PolicyStatement({
+  effect: Effect.ALLOW,
+  actions: ["appsync:GraphQL"],
+  resources: [`${backend.data.resources.graphqlApi.arn}/types/Mutation/*`],
+});
+
+// Create a role for the EventBus to assume
+const eventBusRole = new Role(eventStack, "AppSyncInvokeRole", {
+  assumedBy: new ServicePrincipal("events.amazonaws.com"),
+  inlinePolicies: {
+    PolicyStatement: new PolicyDocument({
+      statements: [policyStatement],
+    }),
+  },
+});
+
+// Create an EventBridge rule to route events to the AppSync API
+const rule = new aws_events.CfnRule(eventStack, "MyOrderRule", {
+  eventBusName: eventBus.eventBusName,
+  name: "broadcastOrderStatusChange",
+  eventPattern: {
+    source: ["amplify.orders"],
+    /* The shape of the event pattern must match EventBridge's event message structure.
+    So, this field must be spelled as "detail-type". Otherwise, events will not trigger the rule.
+
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/ev-events.html
+    */
+    ["detail-type"]: ["OrderStatusChange"],
+    detail: {
+      orderId: [{ exists: true }],
+      status: ["PENDING", "SHIPPED", "DELIVERED"],
+      message: [{ exists: true }],
+    },
+  },
+  targets: [
+    {
+      id: "orderStatusChangeReceiver",
+      arn: backend.data.resources.cfnResources.cfnGraphqlApi
+        .attrGraphQlEndpointArn,
+      roleArn: eventBusRole.roleArn,
+      appSyncParameters: {
+        graphQlOperation: `
+        mutation PublishOrderFromEventBridge(
+          $orderId: String!
+          $status: String!
+          $message: String!
+        ) {
+          publishOrderFromEventBridge(orderId: $orderId, status: $status, message: $message) {
+            orderId
+            status
+            message
+          }
+        }`,
+      },
+      inputTransformer: {
+        inputPathsMap: {
+          orderId: "$.detail.orderId",
+          status: "$.detail.status",
+          message: "$.detail.message",
+        },
+        inputTemplate: JSON.stringify({
+          orderId: "<orderId>",
+          status: "<status>",
+          message: "<message>",
+        }),
+      },
+    },
+  ],
+});
+
 const customNotifications = new CustomNotifications(
   backend.createStack("CustomNotifications"),
   "CustomNotifications",
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,13 +1,80 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import {
+  type ClientSchema,
+  a,
+  defineData,
+  defineFunction,
+} from "@aws-amplify/backend";
 import { sayHello } from "../functions/say-hello/resource";
 import { transcode } from "buffer";
+
+export const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
+
+export const generateHaikuFunction = defineFunction({
+  entry: "./generateHaiku.ts",
+  environment: {
+    MODEL_ID,
+  },
+});
+
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
+
 const schema = a.schema({
+  generateHaiku: a
+    .query()
+    .arguments({ prompt: a.string().required() })
+    .returns(a.string())
+    .authorization((allow) => [allow.authenticated()])
+    .handler(a.handler.function(generateHaikuFunction)),
+  OrderStatus: a.enum(["OrderPending", "OrderShipped", "OrderDelivered"]),
+  OrderStatusChange: a.customType({
+    orderId: a.id().required(),
+    status: a.ref("OrderStatus").required(),
+    message: a.string().required(),
+  }),
+
+  publishOrderToEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
+    .authorization((allow) => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "MyEventBridgeDataSource",
+        entry: "./publishOrderToEventBridge.js",
+      })
+    ),
+  publishOrderFromEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
+    .authorization((allow) => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        entry: "./publishOrderFromEventBridge.js",
+      })
+    ),
+  onOrderFromEventBridge: a
+    .subscription()
+    .for(a.ref("publishOrderFromEventBridge"))
+    .authorization((allow) => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        entry: "./onOrderStatusChange.js",
+      })
+    ),
   sayHello: a
     .query()
     .arguments({
@@ -35,9 +102,20 @@ const schema = a.schema({
       concepto: a.string(),
       categoriaId: a.id(),
       categoria: a.belongsTo("Categoria", "categoriaId"),
-      ubicacion: a.string(),
+      location: a.customType({
+        // fields can be required or optional
+        lat: a.float().required(),
+        long: a.float().required(),
+      }),
     })
     .authorization((allow) => [allow.owner()]),
+  TransaccionesAnalizadas: a
+    .model({
+      concepto: a.string(),
+      categoriaId: a.id(),
+      categoria: a.belongsTo("Categoria", "categoriaId"),
+    })
+    .authorization((allow) => [allow.authenticated()]),
 
   Categoria: a
     .model({
@@ -45,6 +123,10 @@ const schema = a.schema({
       preferencias: a.hasMany("Preferencia", "categoriaId"),
       preferenciasDeclaradas: a.hasMany("PreferenciaDeclarada", "categoriaId"),
       transacciones: a.hasMany("Transaccion", "categoriaId"),
+      transaccionesAnalizadas: a.hasMany(
+        "TransaccionesAnalizadas",
+        "categoriaId"
+      ),
     })
     .authorization((allow) => [allow.authenticated()]),
 
@@ -53,7 +135,6 @@ const schema = a.schema({
       nombre: a.string(),
       categoriaId: a.id(),
       categoria: a.belongsTo("Categoria", "categoriaId"),
-     
     })
     .authorization((allow) => [allow.authenticated()]),
 
