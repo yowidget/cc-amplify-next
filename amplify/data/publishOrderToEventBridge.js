45a7d56d07185b53e5d5c92b6f189278373c1b36
Arturo Vélez Badiola <widget@MacBook-Pro-de-Arturo-2.local>
2024-11-03 21:43:04 -0600

    resolvers EventBridge

---
+++ amplify/data/publishOrderToEventBridge.js
@@ -0,0 +1,16 @@
+export function request(ctx) {
+  return {
+    operation: "PutEvents",
+    events: [
+      {
+        source: "amplify.orders",
+        ["detail-type"]: "OrderStatusChange",
+        detail: { ...ctx.args },
+      },
+    ],
+  };
+}
+
+export function response(ctx) {
+  return ctx.args;
+}
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -108,6 +108,45 @@ const schema = a.schema({
     status: a.ref("OrderStatus").required(),
     message: a.string().required(),
   }),
+    
+  publishOrderToEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({
+        dataSource: "EventBridgeDataSource",
+        entry: "./publishOrderToEventBridge.js",
+      })
+    ),
+  publishOrderFromEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
+    .authorization((allow) => [ allow.authenticated()])
+    .handler(
+      a.handler.custom({
+        entry: "./publishOrderFromEventBridge.js",
+      })
+    ),
+  onOrderFromEventBridge: a
+    .subscription()
+    .for(a.ref("publishOrderFromEventBridge"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({
+        entry: "./onOrderStatusChange.js",
+      })
+    ),
 });
 
 export type Schema = ClientSchema<typeof schema>;
