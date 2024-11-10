fc34259b70b195ee91ea7a473e1912298d2785ef
Arturo Vélez Badiola <widget@192.168.1.65>
2024-11-10 04:40:21 -0600

    error de typescript

--- app/configuracion/recompensas.tsx
+++ app/configuracion/recompensas.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Nullable } from "@aws-amplify/data-schema";
 import { useEffect, useState } from "react";
 import type { Schema } from "@/amplify/data/resource";
@@ -7,7 +8,7 @@ import { uploadData, getUrl } from "aws-amplify/storage";
 
 const client = generateClient<Schema>();
 
-
+// Propiedades del área de entrada
 interface InputAreaProps {
     label: string;
     placeholder: string;
@@ -17,7 +18,7 @@ interface InputAreaProps {
     disabled: boolean;
 }
 
-
+// Componente de área de entrada
 function InputArea({
     label,
     placeholder,
@@ -42,29 +43,28 @@ function InputArea({
     );
 }
 
+// Tipos para la recompensa y categoría
+interface Recompensa {
+    id: string;
+    nombre: Nullable<string>;
+    detalles: Nullable<string>;
+    categoria: {
+        nombre: string;
+        id: string;
+        createdAt: string;
+        updatedAt: string;
+    };
+    img?: string;
+}
+
 export default function Recompensas() {
-
-    const [recompensas, setRecompensas] = useState<
-        {
-            id: string;
-            nombre: Nullable<string>;
-            detalles: Nullable<string>;
-            categoria: {
-                nombre: string; id: string; createdAt: string; updatedAt: string;
-
-            }
-            img?: string;
-        }[]
-    >([]);
+    const [recompensas, setRecompensas] = useState<Recompensa[]>([]);
     const [recompensaInput, setRecompensaInput] = useState<string>("");
-    const [categorias, setCategorias] = useState<
-        Array<Schema["Categoria"]["type"]>
-    >([]);
-
-    //Files
-    const [file, setFile] = useState<File>();
+    const [categorias, setCategorias] = useState<Array<Schema["Categoria"]["type"]>>([]);
+    const [file, setFile] = useState<File | null>(null);
     const [uploading, setUploading] = useState(false);
 
+    // Función para listar categorías
     function listCategorias() {
         try {
             const subscription = client.models.Categoria.observeQuery().subscribe({
@@ -77,42 +77,37 @@ export default function Recompensas() {
         }
     }
 
+    // Función para crear recompensa desde el input
     async function createRecompensaFromInput() {
         if (!file) return alert("Debes seleccionar una imagen");
         const recompensasArray = recompensaInput
             .split(",")
             .map((item) => item.trim())
             .filter(Boolean);
+
         try {
-            recompensasArray.map((recompensa) => {
-                client.models.Recompensa.create({
-                    nombre: recompensa,
-                }).then(async ({ data: recompensa }) => {
-                    if (recompensa) {
-                        console.log(`Recompensa ${recompensa} creada`);
+            recompensasArray.forEach((recompensa) => {
+                client.models.Recompensa.create({ nombre: recompensa })
+                    .then(async ({ data: recompensa }) => {
+                        if (recompensa) {
+                            console.log(`Recompensa ${recompensa.nombre} creada`);
 
-                        const result = await uploadData({
-                            path: `images/${recompensa.id}-${file.name}`,
-                            data: file,
-                            options: {
-                                contentType: "image/png"
-                            }
-                        }).result
+                            const result = await uploadData({
+                                path: `images/${recompensa.id}-${file.name}`,
+                                data: file,
+                                options: { contentType: "image/png" },
+                            }).result;
 
-                        client.models.Recompensa.update({
-                            id: recompensa.id,
-                            img: result?.path,
-                        }).then((recompensaConImagen) => {
-                            console.log("Recompensa con imagen", recompensaConImagen);
-                        });
-
-
-                    }
-
-
-                }).catch((e) => {
-                    console.error(`Error al crear la recompensa ${recompensa}`, e);
-                });
+                            await client.models.Recompensa.update({
+                                id: recompensa.id,
+                                img: result?.path,
+                            });
+                            console.log("Recompensa con imagen actualizada");
+                        }
+                    })
+                    .catch((e) => {
+                        console.error(`Error al crear la recompensa ${recompensa}`, e);
+                    });
             });
             setRecompensaInput("");
             loadRecompensas();
@@ -121,35 +116,28 @@ export default function Recompensas() {
         }
     }
 
+    // Función para cargar recompensas
     function loadRecompensas() {
-        client.models.Recompensa.list(
-            { selectionSet: ['id', 'nombre', 'detalles', 'categoria.*', 'img'] }
-        ).then(({ data, errors }) => {
+        client.models.Recompensa.list({
+            selectionSet: ["id", "nombre", "detalles", "categoria.*", "img"],
+        }).then(({ data, errors }) => {
             if (errors) throw console.error("Error al obtener las recompensas", errors);
-        
-            setRecompensas(data);
+            setRecompensas(data as Recompensa[]);
         });
     }
 
+    // Función para cambiar la categoría de una recompensa
     function handleRecompensaCategoriaChange(id: string, categoriaId: string) {
-        console.log(`Actualizando recompensa ${id} con la categoría ${categoriaId}`);
-
-        client.models.Recompensa.update({
-            id,
-            categoriaId,
-        })
+        client.models.Recompensa.update({ id, categoriaId })
             .then(() => {
-                console.log(
-                    `Recompensa ${id} actualizada con la categoría ${categoriaId}`
-                );
+                console.log(`Recompensa ${id} actualizada con la categoría ${categoriaId}`);
             })
-            .catch((e) => {
-                console.error(`Error al actualizar la recompensa ${id}`, e);
-            });
+            .catch((e) => console.error(`Error al actualizar la recompensa ${id}`, e));
     }
 
-    function handleElminiarRecompensa(id: string) {
-        client.models.Recompensa.delete({ id: id }).then(() => loadRecompensas());
+    // Función para eliminar recompensa
+    function handleEliminarRecompensa(id: string) {
+        client.models.Recompensa.delete({ id }).then(() => loadRecompensas());
     }
 
     useEffect(() => {
@@ -157,51 +145,39 @@ export default function Recompensas() {
         listCategorias();
     }, []);
 
+    // Función para manejar el cambio de archivo
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFile = event.target.files ? event.target.files[0] : null;
-        if (selectedFile) {
-            setFile(selectedFile);
-        }
+        if (selectedFile) setFile(selectedFile);
     };
 
+    // Propiedades de cada recompensa
     interface RecompensaItemProps {
-        id: string;
-        nombre: Nullable<string>;
-        detalles: Nullable<string>;
-        categoria: {
-            nombre: string; id: string; createdAt: string; updatedAt: string;
-
-        }
-        img: string;
+        recompensa: Recompensa;
     }
 
-    const RecompensaItem = ({ recompensa }: { recompensa: RecompensaItemProps }) => {
-        const [url, setURL] = useState<string>('');
+    // Componente de elemento de recompensa
+    const RecompensaItem = ({ recompensa }: RecompensaItemProps) => {
+        const [url, setURL] = useState<string>("");
 
-        getUrl({ path: recompensa.img }).then((url) => {
-            setURL(url.url.href)
-        });
+        useEffect(() => {
+            getUrl({ path: recompensa.img || "" }).then((result) => {
+                setURL(result?.url?.href || "");
+            });
+        }, [recompensa.img]);
 
         return (
-            <div style={{display: 'flex'}}>
-                <div style={{ width: '100%' }}>
+            <div style={{ display: "flex", marginBottom: "10px" }}>
+                <div style={{ width: "100%" }}>
                     <h2>{recompensa.nombre}</h2>
                     <img width={"100px"} src={url} alt="Imagen almacenada" />
-                    <button onClick={() => handleElminiarRecompensa(recompensa.id)}>Eliminar</button>
+                    <button onClick={() => handleEliminarRecompensa(recompensa.id)}>
+                        Eliminar
+                    </button>
                 </div>
             </div>
-        )
-    }
-
-
-    const recompensasList = () => {
-
-        return (
-            <div>
-
-            </div>
-        )
-    }
+        );
+    };
 
     return (
         <section
@@ -213,8 +189,8 @@ export default function Recompensas() {
                 marginTop: "20px",
             }}
         >
-            <div style={{ display: 'flex' }}>
-                <div style={{ width: '50%' }}>
+            <div style={{ display: "flex" }}>
+                <div style={{ width: "50%" }}>
                     <InputArea
                         label="Agregar Recompensas"
                         placeholder="Ingresa las recompensas separadas por coma"
@@ -224,18 +200,10 @@ export default function Recompensas() {
                         disabled={!recompensaInput.trim()}
                     />
                 </div>
-                <div style={{ width: '50%' }}>
+                <div style={{ width: "50%" }}>
                     <input type="file" accept="image/*" onChange={handleFileChange} />
                 </div>
             </div>
-            <InputArea
-                label="Agregar Recompensas"
-                placeholder="Ingresa las recompensas separadas por coma"
-                value={recompensaInput}
-                onChange={(e) => setRecompensaInput(e.target.value)}
-                onSubmit={createRecompensaFromInput}
-                disabled={!recompensaInput.trim()}
-            />
             <h3>Recompensas existentes:</h3>
             <ul>
                 {recompensas.map((recompensa) => (
@@ -243,5 +211,5 @@ export default function Recompensas() {
                 ))}
             </ul>
         </section>
-    )
+    );
 }
