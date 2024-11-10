3a1f54bd783cce99efd145128111d2b663e14312
vBlackBearr <Luispuentes.pgo@outlook.com>
2024-11-10 05:36:03 -0600

    Cambios look recompensas admin

--- app/configuracion/recompensas.tsx
+++ app/configuracion/recompensas.tsx
@@ -8,40 +8,7 @@ import { uploadData, getUrl } from "aws-amplify/storage";
 
 const client = generateClient<Schema>();
 
-// Propiedades del área de entrada
-interface InputAreaProps {
-    label: string;
-    placeholder: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-    onSubmit: () => void;
-    disabled: boolean;
-}
 
-// Componente de área de entrada
-function InputArea({
-    label,
-    placeholder,
-    value,
-    onChange,
-    onSubmit,
-    disabled,
-}: InputAreaProps) {
-    return (
-        <div>
-            <h2>{label}</h2>
-            <textarea
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                style={{ width: "100%", height: "100px", marginBottom: "10px" }}
-            />
-            <button onClick={onSubmit} disabled={disabled}>
-                Crear
-            </button>
-        </div>
-    );
-}
 
 // Tipos para la recompensa y categoría
 interface Recompensa {
@@ -59,10 +26,13 @@ interface Recompensa {
 
 export default function Recompensas() {
     const [recompensas, setRecompensas] = useState<Recompensa[]>([]);
-    const [recompensaInput, setRecompensaInput] = useState<string>("");
+    // const [recompensaInput, setRecompensaInput] = useState<string>("");
     const [categorias, setCategorias] = useState<Array<Schema["Categoria"]["type"]>>([]);
+
+    //File constants
     const [file, setFile] = useState<File | null>(null);
     const [uploading, setUploading] = useState(false);
+
 
     // Función para listar categorías
     function listCategorias() {
@@ -77,44 +47,7 @@ export default function Recompensas() {
         }
     }
 
-    // Función para crear recompensa desde el input
-    async function createRecompensaFromInput() {
-        if (!file) return alert("Debes seleccionar una imagen");
-        const recompensasArray = recompensaInput
-            .split(",")
-            .map((item) => item.trim())
-            .filter(Boolean);
 
-        try {
-            recompensasArray.forEach((recompensa) => {
-                client.models.Recompensa.create({ nombre: recompensa })
-                    .then(async ({ data: recompensa }) => {
-                        if (recompensa) {
-                            console.log(`Recompensa ${recompensa.nombre} creada`);
-
-                            const result = await uploadData({
-                                path: `images/${recompensa.id}-${file.name}`,
-                                data: file,
-                                options: { contentType: "image/png" },
-                            }).result;
-
-                            await client.models.Recompensa.update({
-                                id: recompensa.id,
-                                img: result?.path,
-                            });
-                            console.log("Recompensa con imagen actualizada");
-                        }
-                    })
-                    .catch((e) => {
-                        console.error(`Error al crear la recompensa ${recompensa}`, e);
-                    });
-            });
-            setRecompensaInput("");
-            loadRecompensas();
-        } catch (e) {
-            console.error("Error al crear las recompensas", e);
-        }
-    }
 
     // Función para cargar recompensas
     function loadRecompensas() {
@@ -140,16 +73,76 @@ export default function Recompensas() {
         client.models.Recompensa.delete({ id }).then(() => loadRecompensas());
     }
 
+
+    function CreateRecompensa() {
+
+        const [recompensaName, setRecompensaName] = useState<string>("");
+        const [recompensaDetails, setRecompensaDetails] = useState<string>("");
+
+        // Función para crear recompensa desde el input
+        function createRecompensaFromInput() {
+            if (!file) return alert("Debes seleccionar una imagen");
+
+            client.models.Recompensa.create({ nombre: recompensaName, detalles: recompensaDetails })
+                .then(async ({ data: recompensa }) => {
+                    if (recompensa) {
+                        console.log(`Recompensa ${recompensa.nombre} creada`);
+
+                        const result = await uploadData({
+                            path: `images/${recompensa.id}-${file.name}`,
+                            data: file,
+                            options: { contentType: "image/png" },
+                        }).result;
+
+                        await client.models.Recompensa.update({
+                            id: recompensa.id,
+                            img: result?.path,
+                        });
+                    }
+                })
+                .catch((e) => {
+                    console.error(`Error al crear la recompensa ${recompensaName}`, e);
+                }).finally(() => {
+                    loadRecompensas();
+                    setRecompensaName("");
+                    setRecompensaDetails("");
+                });
+
+        }
+
+        const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const selectedFile = event.target.files ? event.target.files[0] : null;
+            if (selectedFile) setFile(selectedFile);
+        };
+
+        return (
+            <div>
+                <h2>Agregar recompensas</h2>
+                <input
+                    type="text"
+                    value={recompensaName}
+                    onChange={(e) => setRecompensaName(e.target.value)}
+                />
+                <textarea
+                    placeholder={"Ingresa los detalles de la recompensa"}
+                    value={recompensaDetails}
+                    onChange={(e) => setRecompensaDetails(e.target.value)}
+                    style={{ width: "100%", height: "100px", marginBottom: "10px" }}
+                />
+                <input type="file" accept="image/*" onChange={handleFileChange} />
+                <button onClick={() => createRecompensaFromInput()} disabled={!recompensaDetails.trim()}>
+                    Crear
+                </button>
+            </div>
+        );
+    }
+
     useEffect(() => {
         loadRecompensas();
         listCategorias();
     }, []);
 
-    // Función para manejar el cambio de archivo
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFile = event.target.files ? event.target.files[0] : null;
-        if (selectedFile) setFile(selectedFile);
-    };
+
 
     // Propiedades de cada recompensa
     interface RecompensaItemProps {
@@ -166,12 +159,17 @@ export default function Recompensas() {
             });
         }, [recompensa.img]);
 
+
         return (
-            <div style={{ display: "flex", marginBottom: "10px" }}>
-                <div style={{ width: "100%" }}>
-                    <h2>{recompensa.nombre}</h2>
-                    <img width={"100px"} src={url} alt="Imagen almacenada" />
-                    <button onClick={() => handleEliminarRecompensa(recompensa.id)}>
+            <div className="flex items-center p-4 bg-white rounded-lg shadow-md mb-4">
+                <div className="w-full">
+                    <h2 className="text-xl font-bold text-gray-800">{recompensa.nombre}</h2>
+                    <p className="text-gray-600 mb-2">{recompensa.detalles}</p>
+                    <img className="w-24 h-24 object-cover rounded-md mb-2" src={url} alt="Imagen almacenada" />
+                    <button
+                        onClick={() => handleEliminarRecompensa(recompensa.id)}
+                        className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600 transition-colors duration-200"
+                    >
                         Eliminar
                     </button>
                 </div>
@@ -191,21 +189,11 @@ export default function Recompensas() {
         >
             <div style={{ display: "flex" }}>
                 <div style={{ width: "50%" }}>
-                    <InputArea
-                        label="Agregar Recompensas"
-                        placeholder="Ingresa las recompensas separadas por coma"
-                        value={recompensaInput}
-                        onChange={(e) => setRecompensaInput(e.target.value)}
-                        onSubmit={createRecompensaFromInput}
-                        disabled={!recompensaInput.trim()}
-                    />
-                </div>
-                <div style={{ width: "50%" }}>
-                    <input type="file" accept="image/*" onChange={handleFileChange} />
+                    <CreateRecompensa />
                 </div>
             </div>
             <h3>Recompensas existentes:</h3>
-            <ul>
+            <ul style={{ backgroundColor: 'white' }}>
                 {recompensas.map((recompensa) => (
                     <RecompensaItem key={recompensa.id} recompensa={recompensa} />
                 ))}
