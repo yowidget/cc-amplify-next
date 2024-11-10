d1b379ea0245bf1d773ac72fba6dd717f59f774a
Arturo Vélez Badiola <widget@192.168.1.65>
2024-11-10 04:57:58 -0600

    sin estilo extra en configuracion

--- app/configuracion/page.tsx
+++ app/configuracion/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
